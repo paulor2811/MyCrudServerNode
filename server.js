bd3c7fddaaedabe6879701710be2b5dfc72e5b33
Paulo <paulo.ricardo.aleixo3@gmail.com>
2024-05-12 15:36:08 -0300

    Added middleware to enable CORS

--- server.js
+++ server.js
@@ -22,17 +22,12 @@ const dbConfig = {
 app.use(bodyParser.json());
 
 // Middleware para habilitar o CORS
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://my-website-dun-chi.vercel.app', 'https://my-website-dh1skziap-paulos-projects-ede1d64c.vercel.app', 'my-website-git-master-paulos-projects-ede1d64c.vercel.app']; // Adicione aqui os domínios permitidos
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+const cors = require('cors');
+const corsOptions = {
+  origin: 'https://my-website-dh1skziap-paulos-projects-ede1d64c.vercel.app', // Replace with your frontend origin
+  optionsSuccessStatus: 200, // Explicitly set 200 for preflight requests
+}
+app.use(cors(corsOptions));
 
 // Rota para a página inicial
 app.get('/', (req, res) => {
