3c75f2506c88d30af6ac06631a16eb64c61580f7
Paulo <paulo.ricardo.aleixo3@gmail.com>
2024-05-12 14:52:17 -0300

    Added middleware to enable CORS

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 
 // Middleware para habilitar o CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); // Permite solicitações do Angular (localhost:4200)
+    res.setHeader('Access-Control-Allow-Origin', 'https://my-website-flax-pi.vercel.app/'); // Permite solicitações do Angular (localhost:4200)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
