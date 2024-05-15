ac0f77d16b16f68a9d0adcde91948f945b1b176b
Paulo <paulo.ricardo.aleixo3@gmail.com>
2024-05-15 18:31:46 -0300

    Update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2'); // Apenas uma importação do módulo mysql2 agora
+const mysql = require('mysql2');
+const http = require('http');
+const WebSocket = require('ws');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 // Configurações do banco de dados MySQL
 const dbConfig = {
@@ -17,22 +22,15 @@ const dbConfig = {
     }
 };
 
-
-
 app.use(bodyParser.json());
 
 // Middleware para habilitar o CORS
 const cors = require('cors');
 const corsOptions = {
-  origin: 'https://paulos-website.000webhostapp.com', // Replace with your frontend origin
-  optionsSuccessStatus: 200, // Explicitly set 200 for preflight requests
+    origin: 'https://paulos-website.000webhostapp.com',
+    optionsSuccessStatus: 200,
 }
 app.use(cors(corsOptions));
-
-// Rota para a página inicial
-app.get('/', (req, res) => {
-    res.send('Bem-vindo à página inicial');
-});
 
 // Criação da conexão com o banco de dados MySQL
 const connection = mysql.createConnection(dbConfig);
@@ -40,41 +38,31 @@ const connection = mysql.createConnection(dbConfig);
 // Rota para buscar todos os usuários
 app.get('/api/usuarios', (req, res) => {
     console.log(new Date().toLocaleString(), 'Buscando usuários...');
-    
-    // Executar a consulta SQL para buscar todos os usuários
     connection.query('SELECT * FROM usuarios', (error, results) => {
         if (error) {
-            // Se ocorrer algum erro, enviar uma mensagem de erro como resposta
             console.error('Erro ao buscar usuários:', error);
             res.status(500).json({ error: 'Erro ao buscar usuários' });
             return;
         }
-        
         console.log('Usuários encontrados:', results);
-        // Enviar os resultados como resposta da requisição
         res.json(results);
     });
 });
 
 // Rota para registrar um novo usuário
 app.post('/api/usuarios', (req, res) => {
-    const usuario = req.body; // Recupera os dados do usuário do corpo da requisição
+    const usuario = req.body;
     console.log(new Date().toLocaleString(), 'Registrando novo usuário:', usuario);
-    
-    // Executar a consulta SQL para inserir um novo usuário
     connection.query(
         'INSERT INTO usuarios (nome, email, senha, data_de_criacao) VALUES (?, ?, ?, CURRENT_TIMESTAMP)',
         [usuario.nome, usuario.email, usuario.senha],
         (error, results) => {
             if (error) {
-                // Se ocorrer algum erro, enviar uma mensagem de erro como resposta
                 console.error('Erro ao registrar usuário:', error);
                 res.status(500).json({ error: 'Erro ao registrar usuário' });
                 return;
             }
-            
             console.log('Novo usuário registrado com sucesso.');
-            // Enviar uma resposta de sucesso
             res.status(201).json({ message: 'Usuário registrado com sucesso' });
         }
     );
@@ -82,38 +70,27 @@ app.post('/api/usuarios', (req, res) => {
 
 // Rota para fazer login
 app.post('/api/login', (req, res) => {
-    const { email, senha } = req.body; // Recupera os dados de login do corpo da requisição
+    const { email, senha } = req.body;
     console.log(new Date().toLocaleString(), 'Fazendo login para o email:', email);
-    
-    // Executa uma consulta SQL para buscar o usuário com o email fornecido
     connection.query(
         'SELECT * FROM usuarios WHERE email = ?',
         [email],
         (error, results) => {
             if (error) {
-                // Se ocorrer algum erro, envia uma mensagem de erro como resposta
                 console.error('Erro ao fazer login:', error);
                 res.status(500).json({ error: 'Erro ao fazer login' });
                 return;
             }
-            
-            // Verifica se encontrou um usuário com o email fornecido
             if (results.length === 0) {
-                // Se não encontrou um usuário, envia uma mensagem de erro como resposta
                 console.log('Usuário não encontrado.');
                 res.status(401).json({ error: 'Email ou senha incorretos' });
                 return;
             }
-            
-            // Verifica se a senha fornecida coincide com a senha do usuário encontrado
             if (results[0].senha !== senha) {
-                // Se a senha não coincide, envia uma mensagem de erro como resposta
                 console.log('Senha incorreta.');
                 res.status(401).json({ error: 'Email ou senha incorretos' });
                 return;
             }
-            
-            // Se chegou até aqui, significa que o login foi bem-sucedido
             console.log('Login bem-sucedido.');
             res.json({ message: 'Login bem-sucedido', usuario: results[0] });
         }
@@ -122,29 +99,41 @@ app.post('/api/login', (req, res) => {
 
 // Rota para enviar uma nova mensagem
 app.post('/api/mensagens', (req, res) => {
-    const mensagem = req.body; // Recupera os dados da mensagem do corpo da requisição
+    const mensagem = req.body;
     console.log(new Date().toLocaleString(), 'Recebendo nova mensagem:', mensagem);
-    
-    // Executar a consulta SQL para inserir a nova mensagem no banco de dados
     connection.query(
         'INSERT INTO mensagens (usuario, mensagem, data_envio) VALUES (?, ?, CURRENT_TIMESTAMP)',
         [mensagem.user, mensagem.message],
         (error, results) => {
             if (error) {
-                // Se ocorrer algum erro, enviar uma mensagem de erro como resposta
                 console.error('Erro ao inserir mensagem:', error);
                 res.status(500).json({ error: 'Erro ao inserir mensagem' });
                 return;
             }
-            
             console.log('Nova mensagem inserida com sucesso.');
-            // Enviar uma resposta de sucesso
+            // Envia a nova mensagem para todos os clientes conectados via WebSocket
+            wss.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(mensagem));
+                }
+            });
             res.status(201).json({ message: 'Mensagem enviada com sucesso' });
         }
     );
 });
 
+// WebSocket server
+wss.on('connection', function connection(ws) {
+    console.log('Novo cliente conectado.');
+    ws.on('message', function message(data) {
+        console.log('Mensagem recebida do cliente:', data);
+    });
+    ws.on('close', function close() {
+        console.log('Cliente desconectado.');
+    });
+});
+
 // Inicia o servidor na porta especificada
-app.listen(port, () => {
-    console.log(`Servidor rodando em https://mycrudservernode-production.up.railway.app/:${port}`);
+server.listen(port, () => {
+    console.log(`Servidor rodando em https://mycrudservernode-production.up.railway.app:${port}`);
 });
